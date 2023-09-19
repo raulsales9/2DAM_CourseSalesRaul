13d30297b75c6b6ed6288efc29356201ec09945f
ubuntuTests <raulsahe@protonmail.com>
2023-09-19 11:24:27 +0200

    act 11 y act 12

---
+++ first_eva/acts/unit1/ErrorsExcepcions/ex12.py
@@ -0,0 +1,21 @@
+import random
+
+class ErrorEnterMassaGran
+
+class RandomNumber(Exception):
+    try:
+        num = int(input("Introdueix un nombre aleatori entre 0 i 100"))
+        if(num >= 0 && num <= 100){
+            print("El nombre esta dintre del radi, anem a comprobar")
+            random_num = random.randint(1, 100)
+            if(num > random_num){
+                ErrorEnterMassaGran
+            }elif(num < random_num){
+                Error
+            }elif(num == random_num){
+                print("Felicitats, esta correcte")
+            }
+        } else{
+            exce
+        }
+        
