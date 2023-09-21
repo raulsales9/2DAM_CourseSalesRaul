00bb1ecf4b0a7de66c589cba5ee13157f754e8cf
Raul Sales <raulsahe@protonmail.com>
2023-09-21 22:57:27 +0200

    exercici 14 complet mes explicacio

--- first_eva/acts/unit1/POO/activitat14.py
+++ first_eva/acts/unit1/POO/activitat14.py
@@ -6,50 +6,90 @@ class Figura(ABC):
     def area(self) -> float:
         pass
 
+    @abstractmethod
     def perimetre(self) -> float:
         pass
 
 class Cercle(Figura):
     def __init__(self, radi: float):
-        self.radi = radi
+        self._radi = radi
+
+    @property
+    def radi(self):
+        return self._radi
+
+    @radi.setter
+    def radi(self, value):
+        if value < 0:
+            raise ValueError("El radi no pot ser negatiu")
+        self._radi = value
 
     def area(self) -> float:
-        return math.pi * self.radi ** 2
+        return math.pi * self._radi ** 2
 
     def perimetre(self) -> float:
-        return 2 * math.pi * self.radi
+        return 2 * math.pi * self._radi
 
 class Triangle(Figura):
     def __init__(self, costat: float):
-        self.costat = costat
+        self._costat = costat
+
+    @property
+    def costat(self):
+        return self._costat
+
+    @costat.setter
+    def costat(self, value):
+        if value < 0:
+            raise ValueError("El costat no pot ser negatiu")
+        self._costat = value
 
     def area(self) -> float:
-        return (math.sqrt(3) / 4) * self.costat
+        return (math.sqrt(3) / 4) * self._costat
 
     def perimetre(self) -> float:
-        return 3 * self.costat
+        return 3 * self._costat
 
 class Rectangle(Figura):
     def __init__(self, base: float, altura: float):
-        self.base = base 
-        self.altura = altura
+        self._base = base
+        self._altura = altura
+
+    @property
+    def base(self):
+        return self._base
+
+    @base.setter
+    def base(self, value):
+        if value < 0:
+            raise ValueError("La base no pot ser negativa")
+        self._base = value
+
+    @property
+    def altura(self):
+        return self._altura
+
+    @altura.setter
+    def altura(self, value):
+        if value < 0:
+            raise ValueError("L'altura no pot ser negativa")
+        self._altura = value
 
     def area(self) -> float:
-        return self.base * self.altura
+        return self._base * self._altura
 
     def perimetre(self) -> float:
-        return 2 * (self.base + self.altura)
-    
+        return 2 * (self._base + self._altura)
+
 class Quadrat(Rectangle):
     def __init__(self, costat: float):
         super().__init__(costat, costat)
-
+        
 # donem valors per al calcul
 cercle = Cercle(8)
 triangle = Triangle(10)
 rectangle = Rectangle(7, 4)
 quadrat = Quadrat(4)
-
 
 # \n per a imprimir per consola salto linea
 print("\nCercle: ")
---
+++ first_eva/acts/unit1/POO/explicacioAjuda.md
@@ -0,0 +1,5 @@
+**El decorador property s'utilitza per defnir un metode com a getter mitjançant un atribut**
+
+**El decorador @atribut.setter s'utilitza per a a defiir un metode com a setter mitjançant un atribut**
+
+**cal recalcar que a python no existeix els atributs privats com a tal, per aixo l'us de _endavant de el nom de la propietat**
