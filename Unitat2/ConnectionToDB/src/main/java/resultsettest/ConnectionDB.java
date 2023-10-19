bc042a378d1656b8b1739466f3caa140988025e5
RaulSales <96205346+ittsraul@users.noreply.github.com>
2023-10-19 08:09:31 +0200

    sending files about ad

--- Unitat2/ConnectionToDB/src/main/java/resultsettest/ConnectionDB.java
+++ Unitat2/ConnectionToDB/src/main/java/resultsettest/ConnectionDB.java
@@ -1,5 +1,6 @@
-package com.ieseljust.ad.connectiontodb;
+package resultsettest;
 
+import com.ieseljust.ad.connectiontodb.*;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
@@ -65,10 +66,34 @@ class ManageDB {
         }
         return this.conn;
     }
+    
+    public void delete(int deleteRow, String table) throws SQLException{
+        ResultSet rst = this.getConnection().createStatement(
+                    ResultSet.TYPE_FORWARD_ONLY,
+                    ResultSet.CONCUR_UPDATABLE,
+                    ResultSet.CLOSE_CURSORS_AT_COMMIT
+                    ).executeQuery("SELECT * FROM" + table);
+        int row = 0;
+        if(!rst.first()){
+            rst.first();
+        }
+        
+        do{
+            if(row == deleteRow){
+                rst.deleteRow();
+            }
+            row++;
+        }while(rst.next());
+
+    }
 
     public void getSelect(String table) throws SQLException {
         try {
-            ResultSet rst = this.getConnection().createStatement().executeQuery("SELECT * FROM " + table);
+            ResultSet rst = this.getConnection().createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE,
+                    ResultSet.CLOSE_CURSORS_AT_COMMIT
+                    ).executeQuery("SELECT * FROM " + table);
             System.out.println("Content of " + table);
             System.out.println("******************************");
             ResultSetMetaData rsmdQuery = rst.getMetaData();
@@ -84,7 +109,16 @@ class ManageDB {
                 }
                 System.out.println();
             }
-
+//            if(rst.last()){
+//                System.out.println(final);
+//            }
+            while (rst.previous()) {
+                for (int i = 1; i <= rsmdQuery.getColumnCount(); i++) {
+                    System.out.print(String.format("%-25.25s ", rst.getString(i)));
+                }
+                System.out.println();
+            }
+                        
         } catch (SQLException e) {
 
         }
@@ -120,29 +154,31 @@ class ManageDB {
 //        
 //        return rows;
 //    }
-        public int insertStatementPrepared(int gameId,String gameName,String gameDescription,int gameGenere) throws SQLException {
-        int rows = 0;
-//        String SQL
-//                = "INSERT INTO Joc VALUES (1, 'Double Dragon', 'Dos germans bessons experts en "
-//                + "arts marcials s`han de fer camí en un escenari urbà on membres "
-//                + "de bandes rivals volen deixar - los fora de combat.', 1);";
-        //int gameid=2;
-        //String gameName = "Space invaders";
-        //String gameDescription = "";
-        //int gameGenere = 2;
-        String SQL="INSERT INTO Joc Values(?,?,?,?);";
-        PreparedStatement pst = this.getConnection().prepareStatement(SQL);
-        
-        pst.setInt(1,gameId);
-        pst.setString(2,gameName);
-        pst.setString(3,gameDescription);
-        pst.setInt(4,gameGenere);
-        
-        int rst = pst.executeUpdate();
-        
-        System.out.println("Number of rows affected: " + rows);
-        
-        return rows;
+    public void insert(String name, String table) throws SQLException{
+        ResultSet rst = this.getConnection().createStatement(
+                    ResultSet.TYPE_FORWARD_ONLY,
+                    ResultSet.CONCUR_UPDATABLE,
+                    ResultSet.CLOSE_CURSORS_AT_COMMIT
+                    ).executeQuery("SELECT * FROM" + table);
+        if(rst.next()){
+            String jocName = rst.getString("nom");
+            jocName = name + jocName;
+            rst.updateString("nom", "jocName");
+            rst.updateRow();
+        }
+    }
+    public void update(String name, String table) throws SQLException{
+        ResultSet rst = this.getConnection().createStatement(
+                    ResultSet.TYPE_FORWARD_ONLY,
+                    ResultSet.CONCUR_UPDATABLE,
+                    ResultSet.CLOSE_CURSORS_AT_COMMIT
+                    ).executeQuery("SELECT * FROM" + table);
+        while(rst.next()){
+            String jocName = rst.getString("nom");
+            jocName = name + jocName;
+            rst.updateString("nom", "jocName");
+            rst.updateRow();
+        }
     }
 }
 
@@ -156,6 +192,8 @@ public class ConnectionDB {
 
         ManageDB DB = new ManageDB();
         DB.openConnection("jdbc:mysql://localhost", "root", "root");
+        DB.getSelect("Joc");
+        DB.delete(3,"Joc");
 //        DB.getSelect("Genere");
 //        DB.getDBTables();
 //        DB.insertStatementPrepared(Integer.parseInt(args[0]),args[1],args[2],Integer.parseInt(args[3]));
