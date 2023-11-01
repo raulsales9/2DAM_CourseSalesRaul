20e58604f03689b7ae9cf378761ab48ad92294b8
ittsraul <raulsahe@protonmail.com>
2023-11-01 23:32:41 +0100

    updating the game
    problems with the restart and the colors

--- PrimeraEvaluacio/acts/unitat3/entregables/joc.py
+++ PrimeraEvaluacio/acts/unitat3/entregables/joc.py
@@ -3,35 +3,60 @@ import random
 
 class App:
     def __init__(self, num_items: int) -> None:
-        self.num_items = num_items
-        self.target_number = random.randint(1, num_items)
-        print(f"El número objetivo es: {self.target_number}")
-        ft.app(target=self.main)
+        self._num_items = num_items
+        self._target_number = random.randint(1, num_items)
+        print(f"El número objectiu és: {self._target_number}")
+        ft.app(target=self.start)
 
-    def main(self, page: ft.Page):
+    @property
+    def num_items(self):
+        return self._num_items
+
+    @num_items.setter
+    def num_items(self, value):
+        self._num_items = value
+
+    @property
+    def target_number(self):
+        return self._target_number
+
+    @target_number.setter
+    def target_number(self, value):
+        self._target_number = value
+
+    def start(self, page: ft.Page):
         self.page = page
+        self.page.add(ft.ElevatedButton("Comença el joc", on_click=self.main))
+
+    def main(self, args=None):
+        self.page.clean()
         self.buttons = [] 
         row = ft.Row(
             wrap=True,
             spacing=10,
             run_spacing=10,
-            # generem 30 items
-            # controls=self.items(30)
-            # pasem de forma dinamica el numero d'items mitjançant App(25), per lo tant aquesta manera no hauriem d'establir cada vegada la cantitat d'items
             controls=self.items(self.num_items),
-            width=page.window_width,
+            width=self.page.window_width,
         )
 
-        self.page.add(
-            ft.Column(
+        # Afegeix una vista per a aquesta ruta
+        self.page.views.append(
+            ft.View(
+                "/items",
                 [
-                    ft.Text(
-                        "Change the row width to see how child items wrap onto multiple rows:"
-                    ),
-                    row,
+                    ft.Column(
+                        [
+                            ft.Text(
+                                "Canvia l'amplària de la fila per veure com els elements fills es distribueixen en diverses files:"
+                            ),
+                            row,
+                        ]
+                    )
                 ]
             )
         )
+
+        self.page.go("/items")
 
     def items(self, count):
         items = []
@@ -44,42 +69,59 @@ class App:
                 bgcolor=ft.colors.AMBER,
                 border_radius=ft.border_radius.all(5),
             )
-            button.on_click = self.check_number(i)
+            button.on_click = self.check_number(i, button)
             items.append(button)
             self.buttons.append(button) 
         return items
 
-    def check_number(self, number: int):
+    def check_number(self, number: int, button: ft.Container):
         def on_click(args):
-            # print(self.control.content.value)
             if number == self.target_number:
+                button.bgcolor = ft.colors.GREEN
                 self.page.clean()
                 self.page.add(ft.Text("Has trobat el número!"))
-                for button in self.buttons:
-                    button.enabled = False
-                self.page.add(ft.ElevatedButton("Reiniciar", on_click=self.restart))
-            else:
+                for btn in self.buttons:
+                    btn.enabled = False
                 
-                # Cambia la ruta de la página al número clicado
-                self.page.go(f"/{number}")
-                self.page.add(ft.Text(f"El número {number} no és el número buscat."))
-                # Añade una vista para esta ruta
+                # Afegeix una vista per a aquesta ruta
                 self.page.views.append(
                     ft.View(
                         f"/{number}",
                         [
                             ft.AppBar(title=ft.Text(f"Número {number}"), bgcolor=ft.colors.SURFACE_VARIANT),
-                            ft.ElevatedButton("Volver", on_click=lambda _: self.page.go("/")),
+                            ft.Container(bgcolor=ft.colors.GREEN, width=100, height=100),
+                            ft.ElevatedButton("Tornar", on_click=lambda _: self.main()),
                         ],
                     )
                 )
+                
+            else:
+                button.bgcolor = ft.colors.RED
+               
+                # Afegeix una vista per a aquesta ruta
+                self.page.views.append(
+                    ft.View(
+                        f"/{number}",
+                        [
+                            ft.AppBar(title=ft.Text(f"Número {number}"), bgcolor=ft.colors.SURFACE_VARIANT),
+                            ft.Container(bgcolor=ft.colors.RED, width=100, height=100),
+                            ft.ElevatedButton("Tornar", on_click=lambda _: self.main()),
+                        ],
+                    )
+                )
+                
+            self.page.go(f"/{number}")
+               
         return on_click
 
     def restart(self, args=None):
-        self.page.clean()
-        self.target_number = random.randint(1, self.num_items)
-        print(f"El nuevo número objetivo es: {self.target_number}") 
-        self.main(self.page)
+        # Reinicia el joc generant un nou número objectiu i tornant a la pàgina principal.
+        self._target_number = random.randint(1, self._num_items)
+        print(f"El nou número objectiu és: {self._target_number}")
+        for btn in self.buttons:
+            btn.enabled = True
+            btn.bgcolor = ft.colors.AMBER
+        self.main()
 
 if __name__ == "__main__":
     app = App(25)
