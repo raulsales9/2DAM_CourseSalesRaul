a58fb45903b6a7d8541d801571bedaf9397308dd
ittsraul <raulsahe@protonmail.com>
2023-11-03 09:06:55 +0100

    code with the game logic and the new color theme

--- PrimeraEvaluacio/acts/unitat3/entregables/joc.py
+++ PrimeraEvaluacio/acts/unitat3/entregables/joc.py
@@ -1,10 +1,15 @@
 import flet as ft
 import random
+from colours import light_theme, dark_theme
 
 class App:
     def __init__(self, num_items: int) -> None:
         self._num_items = num_items
         self._target_number = random.randint(1, num_items)
+        self.pressed_numbers = []
+        self.theme = light_theme
+        self.game_won = False 
+        self.button_states = [ft.colors.AMBER] * num_items  
         print(f"El número objectiu és: {self._target_number}")
         ft.app(target=self.start)
 
@@ -26,9 +31,9 @@ class App:
 
     def start(self, page: ft.Page):
         self.page = page
-        self.page.title = "Troba el número"  # Afegeix el títol a la pàgina
+        self.page.title = "Troba el número" 
         self.page.add(ft.ElevatedButton("Comença el joc", on_click=self.main))
-
+    
     def main(self, args=None):
         self.page.clean()
         self.buttons = [] 
@@ -40,26 +45,26 @@ class App:
             width=self.page.window_width,
         )
 
-        # Afegeix una vista per a aquesta ruta
+        if self.game_won:
+            controls = [row, ft.ElevatedButton("Reiniciar", on_click=self.restart)]
+        else:
+            controls = [row]
+
         self.page.views.append(
             ft.View(
                 "/items",
                 [
                     ft.Column(
                         [
-                            ft.Text(
-                                "Numeros :"
-                            ),
-                            row,
-                            ft.ElevatedButton("Reiniciar", on_click=self.restart),  # Botón para reiniciar el juego
-                        ]
+                            ft.Text("Numeros :"),
+                        ] + controls
                     )
                 ]
             )
         )
 
         self.page.go("/items")
-
+        
     def items(self, count):
         items = []
         for i in range(1, count + 1):
@@ -68,65 +73,52 @@ class App:
                 alignment=ft.alignment.center,
                 width=140,
                 height=150,
-                bgcolor=ft.colors.AMBER,
+                bgcolor=self.button_states[i - 1],  
                 border_radius=ft.border_radius.all(5),
             )
             button.on_click = self.check_number(i, button)
             items.append(button)
             self.buttons.append(button) 
         return items
-
+    
     def check_number(self, number: int, button: ft.Container):
         def on_click(args):
+            self.pressed_numbers.append(number) 
             if number == self.target_number:
+                self.game_won = True
                 button.bgcolor = ft.colors.GREEN
-                for btn in self.buttons:
-                    btn.enabled = False
-                self.page.clean()
-                self.page.add(ft.Text("Has trobat el número!"))
-                self.page.add(ft.ElevatedButton("Reiniciar", on_click=self.restart))  # Botón para reiniciar el juego
-
-                # Afegeix una vista per a aquesta ruta
-                self.page.views.append(
-                    ft.View(
-                        f"/{number}",
-                        [
-                            ft.AppBar(title=ft.Text(f"Número {number}"), bgcolor=ft.colors.SURFACE_VARIANT),
-                            ft.Container(bgcolor=ft.colors.GREEN, width=100, height=100),
-                            ft.ElevatedButton("Tornar", on_click=lambda _: self.main()),
-                        ],
-                    )
-                )
-
+                self.button_states[number - 1] = ft.colors.GREEN 
             else:
                 button.bgcolor = ft.colors.RED
-                button.enabled = False  # Deshabilita el botón si el número no es correcto
+                self.button_states[number - 1] = ft.colors.RED 
+            button.enabled = False
 
-                # Afegeix una vista per a aquesta ruta
-                self.page.views.append(
-                    ft.View(
-                        f"/{number}",
-                        [
-                            ft.AppBar(title=ft.Text(f"Número {number}"), bgcolor=ft.colors.SURFACE_VARIANT),
-                            ft.Container(bgcolor=ft.colors.RED, width=100, height=100),
-                            ft.ElevatedButton("Tornar", on_click=lambda _: self.main()),
-                        ],
-                    )
+            # Afegeix una vista per a aquesta ruta
+            self.page.views.append(
+                ft.View(
+                    f"/{number}",
+                    [
+                        ft.AppBar(title=ft.Text(f"Número {number}"), bgcolor=ft.colors.SURFACE_VARIANT),
+                        ft.Container(bgcolor=ft.colors.RED if number != self.target_number else ft.colors.GREEN, width=100, height=100),
+                        ft.Text("Incorrecte" if number != self.target_number else "Correcto"),
+                        ft.ElevatedButton("Tornar", on_click=lambda _: self.main()),
+                    ],
                 )
+            )
 
             self.page.go(f"/{number}")
-
         return on_click
 
+
     def restart(self, args=None):
-        # Reinicia el joc generant un nou número objectiu i tornant a la pàgina principal.
         self._target_number = random.randint(1, self._num_items)
         print(f"El nou número objectiu és: {self._target_number}")
-        for btn in self.buttons:
+        for i, btn in enumerate(self.buttons):
             btn.enabled = True
-            btn.bgcolor = ft.colors.AMBER
+            btn.bgcolor = ft.colors.AMBER  
+            self.button_states[i] = ft.colors.AMBER 
+        self.game_won = False 
         self.main()
 
 if __name__ == "__main__":
     app = App(25)
-    app.main()
