aa160e5e9a9314574a3ce330dd73f34a2385b8b5
ittsraul <raulsahe@protonmail.com>
2023-11-09 16:26:10 +0100

    conecting the client with the server

--- first_evaluation/pspdme/whatsdammlocal/WhatsDAM_Java/client/src/main/java/com/ieseljust/psp/client/communications/communicationManager.java
+++ first_evaluation/pspdme/whatsdammlocal/WhatsDAM_Java/client/src/main/java/com/ieseljust/psp/client/communications/communicationManager.java
@@ -1,6 +1,7 @@
 package com.ieseljust.psp.client.communications;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -17,45 +18,50 @@ import org.json.JSONObject;
 
 public class communicationManager {
 
-    /* Aquesta classe s'encarrega de la gestió de la
-     comunicació amb el servidor.
+    /*
+     * Aquesta classe s'encarrega de la gestió de la
+     * comunicació amb el servidor.
      */
-    
-    public static JSONObject sendServer(String msg) {
+
+    public static JSONObject sendServer(String msg) throws IOException {
 
         // TO-DO:
         // Envía al servidor l'string msg
         // I retorna un JSON amb la resposta
-        
-        Socket socket=new Socket();
-        InetSocketAddress socketAddr=new InetSocketAddress(CurrentConfig.server(), CurrentConfig.port());
+
+        Socket socket = new Socket();
+        InetSocketAddress socketAddr = new InetSocketAddress(CurrentConfig.server(), CurrentConfig.port());
 
         try {
             socket.connect(socketAddr);
             // Connexió realitzada amb èxit
             // Obtenció dels streams d'entrada i eixida
-            InputStream is=socket.getInputStream();
-            OutputStream os=socket.getOutputStream();
-
+            InputStream is = socket.getInputStream();
+            OutputStream os = socket.getOutputStream();
 
             // Creem fluxos per a la lectura i escriptura de caràcters
-            InputStreamReader isr=new InputStreamReader(is);
-            OutputStreamWriter osw=new OutputStreamWriter(os);
+            InputStreamReader isr = new InputStreamReader(is);
+            OutputStreamWriter osw = new OutputStreamWriter(os);
 
             // Creem fluxos per a la lectura i escriptura de línies
-            BufferedReader bReader=new BufferedReader(isr);
-            PrintWriter pWriter=new PrintWriter(osw);
+            BufferedReader bReader = new BufferedReader(isr);
+            PrintWriter pWriter = new PrintWriter(osw);
 
             // Escrivim al socket el missatge
-            pWriter.println(msg);
+            pWriter.println("Envie al server: " + msg);
             pWriter.flush();
 
-            JSONObject resposta = null ;
+            pWriter.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+            JSONObject resposta = null;
             String linia;
-            while ((linia=bReader.readLine()) != null ){
+            pWriter.println("bbbbbbbbbbbbbbbbbbb");
+            while ((linia = bReader.readLine()) != null) {
                 resposta = new JSONObject(linia);
             }
-            
+            pWriter.println("ccccccccccccccccccccccc");
+
+            System.out.println("Rep: " + linia);
+
             pWriter.close();
             bReader.close();
             isr.close();
@@ -66,55 +72,57 @@ public class communicationManager {
             socket.close();
             return new JSONObject(resposta);
         } catch (IOException e) {
-            System.out.println("Excepció en la connexió: "+e.getMessage());
-            return new JSONObject("{'status':'error', 'message':'"+e.getMessage()+"'}");
-        
-        }  
-        
+            System.out.println("Excepció en la connexió: " + e.getMessage());
+            return new JSONObject("{'status':'error', 'message':'" + e.getMessage() + "'}");
+
+        }
+
     }
 
     public static void connect() throws JSONException, communicationManagerException {
 
         // TO-DO:
 
-        // Creem un misstge pe al servidor amb l'ordre (command) register, 
-        // el nom d'usuari (user) i el port (listenPost) pel qual el client escoltarà 
+        // Creem un misstge pe al servidor amb l'ordre (command) register,
+        // el nom d'usuari (user) i el port (listenPost) pel qual el client escoltarà
         // les notificacions (el tenim a CurrentConfig.listenPort())
 
         // Enviarà el missatge al servidor a través de sendServer.
 
         // Si es produeix un error, llançarà una excepció i aturarà
         // l'aplicaió (per exemple, si l'usuari ja existeix al servidor)
-        // Teniu per a això l'excepció communicationManagerException 
-        // com a excepció personalitzada al projecte. 
-        try{
-            
-            //messaje per al servidor (command) register, 
-            //user, port (listenPost)
-            //currentConfig.listenPort()
+        // Teniu per a això l'excepció communicationManagerException
+        // com a excepció personalitzada al projecte.
+        try {
+
+            // messaje per al servidor (command) register,
+            // user, port (listenPost)
+            // currentConfig.listenPort()
+            System.out.println("1111111111111111111111111111111111111111");
             JSONObject newmsg = new JSONObject();
             newmsg.put("command", "register");
             newmsg.put("user", CurrentConfig.username());
             newmsg.put("listenPort", CurrentConfig.listenPort());
 
+            System.out.println(newmsg.toString());
             JSONObject resposta = sendServer(newmsg.toString());
 
-            if(resposta.getString("status").equals("error")){
-            
-            }else{
+            if (resposta.getString("status").equals("error")) {
+
+            } else {
                 throw new communicationManagerException(resposta.getString("message"));
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public static void sendMessage(Message m){
+    public static void sendMessage(Message m) {
         // Envia un misstge al servidor (es fa amb una línia!)
         try {
             sendServer(m.toJSONCommand().toString());
         } catch (IOException e) {
             System.out.println("Error al enviar el mensaje: " + e.getMessage());
         }
-    }    
+    }
 }
--- first_evaluation/pspdme/whatsdammlocal/WhatsDAM_Java/client/src/main/java/com/ieseljust/psp/client/communications/serverListener.java
+++ first_evaluation/pspdme/whatsdammlocal/WhatsDAM_Java/client/src/main/java/com/ieseljust/psp/client/communications/serverListener.java
@@ -39,48 +39,85 @@ public class serverListener implements Runnable {
     int listenerPort = CurrentConfig.listenPort();
 
     @Override
-    public void run() {
+public void run() {
+    // 1. Crear un socket de tipo servidor que escuche por el puerto de recepción de mensajes
+    ServerSocket listener = null;
+    try {
+        // Crear el socket en un puerto determinado por el sistema
+        // y guardarlo en listenPort.
+        listener = new ServerSocket(0);
+        CurrentConfig.setlistenPort(listener.getLocalPort());
+
+        while (true) {
+            try {
+                // 3. Crear un socket de tipo servidor que escuche por el puerto de recepción de mensajes
+                Socket client = listener.accept();
+                InputStream is = client.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is);
+                BufferedReader br = new BufferedReader(isr);
+
+                String line;
+                while ((line = br.readLine()) != null) {
+                    processNotification(line);
+                }
+
+                client.close();
+            } catch (IOException e) {
+                System.out.println("La aceptación ha fallado.");
+            }
+        }
+    } catch (IOException e) {
+        System.out.println("El puerto " + listenerPort + " está ocupado o es inaccesible.");
+        return;
+    }
+}
+
+private void processNotification(String notification) {
+    try {
+        JSONObject json = new JSONObject(notification);
+        String type = json.getString("type");
+
+        if ("userlist".equals(type)) {
+            JSONArray userList = json.getJSONArray("content");
+            ArrayList<String> users = new ArrayList<>();
+            for (int i = 0; i < userList.length(); i++) {
+                users.add(userList.getString(i));
+            }
+            vm.updateUserList(users);
+        } else if ("message".equals(type)) {
+            String user = json.getString("user");
+            String content = json.getString("content");
+            vm.addMessage(new Message(user, content));
+        }
+    } catch (Exception e) {
+        System.out.println("Error al procesar notificación: " + e.getMessage());
+    }
+}
+
         // 1. Crear un socket de tipus servidor que escolte pel port de recepció de
         // missatges
-        ServerSocket listener = null;
-        try {
-            // Creem el socket en un  port determinat pel sistema
+       
+            // Creem el socket en un port determinat pel sistema
             // i el guardem a listenPort.
-            listener = new ServerSocket(0);
-            CurrentConfig.setlistenPort(listener.getLocalPort());
-            while (true) {
-                try {
+           
                     // 3. Creem un socket de tipus servidor que escolte pel port de recepció de
                     // missatges
-                    Socket client = listener.accept();
-                    // 4. Creem un thread per a processar la connexió
-                    new Thread(new clientHandler(client, vm)).start();
-                } catch (IOException e) {
-                    System.out.println("L'acceptació ha fallat.");
-                }
-            }
-
-        } catch (IOException e) {
-            System.out.println("El port " + listenerPort + " està ocupato és inaccessible.");
-            return;
-        }
+                    
+               
 
         // TO-DO
         // 2. Iniciem un bucle infinit a l'espera de rebre connexions
         // Quan arribe una connexió la processrem de manera adeqüada
         // Les peticions que podme rebre seran de tipus:
-        
-        // {"type": "userlist", "content": [Llista d'usuaris]}, amb un JSONArray amb la llista d'usuaris.
+
+        // {"type": "userlist", "content": [Llista d'usuaris]}, amb un JSONArray amb la
+        // llista d'usuaris.
         // {"type": "message", "user":"usuari", "content": "Contingut del missatge" }
-        
+
         // És interessant implementar un mètode a banda per processat aquestes línies
         // però no cal que creem un fil a propòsit per atendre cada missatge, ja que
-        // no som un servidor com a tal, i el que estem fent aci, és mantindre un 
+        // no som un servidor com a tal, i el que estem fent aci, és mantindre un
         // canal de recepció només amb el servidor.
-
-        
-        
-        
-    }
+    
 
 }
