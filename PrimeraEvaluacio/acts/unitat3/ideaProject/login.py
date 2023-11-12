8c5c6e8ec2dadf31ca072372d35824cae88cc8fb
ittsraul <raulsahe@protonmail.com>
2023-11-13 00:40:34 +0100

    password hash done

--- PrimeraEvaluacio/acts/unitat3/ideaProject/login.py
+++ PrimeraEvaluacio/acts/unitat3/ideaProject/login.py
@@ -10,14 +10,12 @@ def main(page: ft.Page):
     pw = ft.Text(bottom=50, right=50, style="displaySmall")
     page.overlay.append(pw)
 
-    # Crear los campos del formulario
     txt_nombre = ft.TextField(label="Nombre", border="underline")
     txt_correo = ft.TextField(label="Correo", border="underline")
     txt_password = ft.TextField(label="Password", password=True, border="underline")
     btn_send = ft.ElevatedButton("Login", bgcolor=ft.colors.BLUE_300)
     link_registro = ft.Text("Don't have an account? Register here",) #url="#")
 
-    # Agregar los campos a un contenedor
     form_container = ft.Container(
         ft.Column([
             ft.Text("Come with the best Social network", size=20, weight="bold",  ),
@@ -35,7 +33,6 @@ def main(page: ft.Page):
         bgcolor=ft.colors.WHITE,
         col={"sm": 12, "md": 10, "xl": 6},
     )
-    #form_container.alignment = alignment.center
 
     page.add(
         ft.ResponsiveRow(
--- PrimeraEvaluacio/acts/unitat3/ideaProject/register.py
+++ PrimeraEvaluacio/acts/unitat3/ideaProject/register.py
@@ -1,5 +1,5 @@
 import flet as ft
-
+from passlib.hash import pbkdf2_sha256
 def main(page: ft.Page):
     def page_resize(e):
         pw.value = f"{page.width} px"
@@ -13,9 +13,11 @@ def main(page: ft.Page):
     # Crear los campos del formulario
     txt_nombre = ft.TextField(label="Nombre", border="underline")
     txt_correo = ft.TextField(label="Correo", border="underline")
-    txt_password = ft.TextField(label="Password", password=True, border="underline")
-    btn_send = ft.ElevatedButton("Login", bgcolor=ft.colors.BLUE_300)
+    btn_send = ft.ElevatedButton("SignIn", bgcolor=ft.colors.BLUE_300)
     link_registro = ft.Text("Don't have an account? Register here",) #url="#")
+    user = ft.TextField(label="nombre de usuario")
+    passwd = ft.TextField(label="Contraseña ", password=True, can_reveal_password=False)
+    passwd2 = ft.TextField(label="Repite la contraseña", password=True, can_reveal_password=False)
 
     # Agregar los campos a un contenedor
     form_container = ft.Container(
@@ -24,7 +26,9 @@ def main(page: ft.Page):
             ft.Divider(),
             txt_correo,
             txt_nombre,
-            txt_password,
+            user,
+            passwd,
+            passwd2,
             btn_send,
             link_registro
         ]),
@@ -49,5 +53,18 @@ def main(page: ft.Page):
             ],
         ),
     )
+
+    def hash_password(password):
+        return pbkdf2_sha256.hash(password, rounds=20000, salt_size=16)
+    
+    def verify_password(password):
+        return pbkdf2_sha256.verify(password, hash)
+    
+    def on_button_click(e):
+        hashed_password = hash_password(passwd.value)
+        print(f"Hashed password: {hashed_password}")
+
+    btn_send.on_click = on_button_click
+
 
 ft.app(target=main)
