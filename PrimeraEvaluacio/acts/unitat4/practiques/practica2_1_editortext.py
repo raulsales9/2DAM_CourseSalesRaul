79a2d1242cd74f712c8ec0bc1634f0caaf8aee5a
RaulSales <raulsahe@protonmail.com>
2023-12-20 09:19:19 +0100

    text_editor:done

--- PrimeraEvaluacio/acts/unitat4/practiques/practica2_1_editortext.py
+++ PrimeraEvaluacio/acts/unitat4/practiques/practica2_1_editortext.py
@@ -1,5 +1,5 @@
-from PySide6.QtWidgets import QMainWindow, QTextEdit, QAction, QFileDialog, QApplication
-from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QMainWindow, QTextEdit, QFileDialog, QApplication, QMessageBox
+from PySide6.QtGui import QIcon, QAction, QKeySequence
 import sys
 import os
 
@@ -9,18 +9,22 @@ class Editor(QMainWindow):
 
         self.text = QTextEdit(self)
         self.setCentralWidget(self.text)
-
-        self.open_action = QAction(QIcon('open.png'), 'Obrir', self)
+        # imatges
+        save = os.path.join(os.path.dirname(__file__), "assets/img/files-and-folders.png")
+        close = os.path.join(os.path.dirname(__file__), "assets/img/cerrar-sesion.png")
+        open = os.path.join(os.path.dirname(__file__), "assets/img/disquete.png")
+        self.open_action = QAction(QIcon(open), 'Obrir', self)
+        #shortcut per a tecles especials i controls
         self.open_action.setShortcut('Ctrl+O')
         self.open_action.triggered.connect(self.open_file)
-
-        self.save_action = QAction(QIcon('save.png'), 'Guardar', self)
+       
+        self.save_action = QAction(QIcon(save), 'Guardar', self)
         self.save_action.setShortcut('Ctrl+S')
         self.save_action.triggered.connect(self.save_file)
 
-        self.exit_action = QAction(QIcon('exit.png'), 'Sortir', self)
+        self.exit_action = QAction(QIcon(close), 'Sortir', self)
         self.exit_action.setShortcut('Ctrl+Q')
-        self.exit_action.triggered.connect(self.close)
+        self.exit_action.triggered.connect(self.close_program)
 
         self.toolbar = self.addToolBar('Toolbar')
         self.toolbar.addAction(self.open_action)
@@ -32,7 +36,13 @@ class Editor(QMainWindow):
         self.file_menu.addAction(self.save_action)
         self.file_menu.addAction(self.exit_action)
 
-    def open_file(self):
+    def open_file(self): 
+        if self.text.document().isModified():
+            tanca = QMessageBox.question(self, "Eixir", "Estas segur que vols perdre el teu treball?", QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel)
+            if tanca == QMessageBox.Save:
+                self.save_file()
+            elif tanca == QMessageBox.Discard:
+                self.text.clear()
         options = QFileDialog.Options()
         filename, _ = QFileDialog.getOpenFileName(self, 'Obrir', '', 'Text Files (*.txt)', options=options)
         if filename:
@@ -45,6 +55,27 @@ class Editor(QMainWindow):
         if filename:
             with open(filename, 'w') as f:
                 f.write(self.text.toPlainText())
+                
+    def close_file(self):
+        if self.text.document().isModified():
+            tanca = QMessageBox.question(self,"Eixir", "Estas segur que vols perdre el teu treball?", QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel)
+            if tanca == QMessageBox.Save:
+                self.save_file()
+            elif tanca == QMessageBox.Discard:
+                self.close()
+        else:
+            self.close()
+    
+    def close_program(self):
+        if self.text.document().isModified():
+            tanca = QMessageBox.question(self,"Eixir", "Estas segur que vols perdre el teu treball?", QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel)
+            if tanca == QMessageBox.Save:
+                self.save_file()
+                self.close()
+            elif tanca == QMessageBox.Discard:
+                self.close()
+        else:
+            self.close()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
